22173e5587d3dd73b4fee6c6a511768efaaba535
jrotolo <jrotol5@lsu.edu>
2013-09-13 20:27:09 +0000

    Signup page done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,15 +4,20 @@ describe User do
 
 	# before block runs before the test
   before do
-  	@user = User.new(name: "Example User", email: "user@example.com")
+  	@user = User.new(name: "Example User", email: "user@example.com",
+  										password: "foobars", password_confirmation: "foobars")
   end
 
   # subject {} block makes the argument the subject for the test
   subject { @user }
 
-  # test for the existence of a name and email
+  # test for the existence of a name, email, and password_digest columns
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   # check for validation testing
   it { should be_valid }
@@ -32,9 +37,43 @@ describe User do
   	it { should_not be_valid }
   end
 
+  describe "when password is not present" do 
+  	before do 
+  		@user = User.new(name: "Example User", email: "user@example.com",
+  											password: " ", password_confirmation: " ")
+  	end
+  	it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do 
+  	before { @user.password_confirmation = "mismatch" }
+  	it { should_not be_valid }
+  end
+
+  describe "return value of authenticate method" do 
+  	before { @user.save }
+  	let(:found_user) { User.find_by(email: @user.email) }
+
+  	describe "with valid password" do 
+  		it { should eq found_user.authenticate(@user.password) }
+  	end
+
+  	describe "with invalid password" do 
+  		let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+  		it { should_not eq user_for_invalid_password }
+  		specify { expect(user_for_invalid_password).to be_false }
+  	end
+  end
+
+  describe "with a password that's too short" do 
+  	before { @user.password = @user.password_confirmation = "a"*5 }
+  	it { should be_invalid }
+  end
+
   describe "when email format is invalid" do 
   	it "should be invalid" do 
-  		addressses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
+  		addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
   		addresses.each do |invalid_address|
   			@user.email = invalid_address
   			expect(@user).not_to be_valid
@@ -44,11 +83,31 @@ describe User do
 
   describe "when email format is valid" do 
   	it "should be valid" do 
-  		addressses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+  		addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
   		addresses.each do |valid_address|  
   			@user.email = valid_address
   			expect(@user).to be_valid
   		end
+  	end
+  end
+
+  describe "when email address is already taken" do 
+  	before do 
+  		user_with_same_email = @user.dup
+  		user_with_same_email.email = @user.email.upcase
+  		user_with_same_email.save
+  	end
+
+  	it { should_not be_valid }
+  end
+
+  describe "email address with mixed case" do 
+  	let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+  	it "should be save as all lower-case" do 
+  		@user.email = mixed_case_email
+  		@user.save
+  		expect(@user.reload.email).to eq mixed_case_email.downcase
   	end
   end
 end
