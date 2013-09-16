731fd6d89ffc89e15eba39e62f2bf5693307b75e
jrotolo <jrotol5@lsu.edu>
2013-09-16 18:44:44 +0000

    Added remeber_token column to users db

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,18 @@
 class SessionsController < ApplicationController
+	# force_ssl
 
 	def new
 	end
 
 	def create
+		user = User.find_by(email: params[:email].downcase)
+		if user && user.authenticate(params[:password])
+			sign_in user
+			redirect_to user
+		else
+			flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+			render 'new'
+		end
 	end
 
 	def destroy
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,8 +9,14 @@ describe "Authentication" do
 
   	describe "with invalid information" do
   		before { click_button "Sign in" }
+
   		it { should have_title('Sign in') }
   		it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+  		describe "after visiting another page" do 
+  			before { click_link "Home" }
+  			it { should_not have_selector('div.alert.alert-error') }
+  		end
   	end
 
   	describe "with valid information" do 
@@ -21,10 +27,12 @@ describe "Authentication" do
   			click_button "Sign in"
   		end
 
+
+
   		it { should have_title(user.name) }
-  		it { should have_link('Profile',	href: user_path(user)) }
-  		it { should have_link('Sign out',	href: singout_path) }
-  		it { should_not have_link('Sign in',	href: signin_path }
+  		it { should have_link('Profile',			href: user_path(user)) }
+  		it { should have_link('Sign out',			href: singout_path) }
+  		it { should_not have_link('Sign in',	href: signin_path) }
   	end
   end
 end
